02ac241f8dea64dd4e98110c8573596e281678bc
ElinEunjung <eunjungp615@gmail.com>
2025-12-10 15:23:20 +0900

    update files

---
+++ Check-service/src/main/java/com/example/frostbyte/checkservice/CheckServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.frostbyte.checkservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckServiceApplication.class, args);
+	}
+
+}
