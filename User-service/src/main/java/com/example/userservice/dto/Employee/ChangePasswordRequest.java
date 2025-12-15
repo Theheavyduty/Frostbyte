0ea7de3220876ad0d49801b36cd4d59d61146b5a
Theheavyduty <131966491+Theheavyduty@users.noreply.github.com>
2025-12-15 10:46:48 +0100

    Delete User-service directory

--- User-service/src/main/java/com/example/userservice/dto/Employee/ChangePasswordRequest.java
+++
@@ -1,8 +0,0 @@
-package com.example.userservice.dto.Employee;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record ChangePasswordRequest(
-        @NotBlank String oldPassword,
-        @NotBlank String newPassword
-) { }
--- User-service/src/main/java/com/example/userservice/dto/Employee/EmployeeLoginRequest.java
+++
@@ -1,8 +0,0 @@
-package com.example.userservice.dto.Employee;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record EmployeeLoginRequest(
-        @NotBlank String name,
-        @NotBlank String password
-) { }
