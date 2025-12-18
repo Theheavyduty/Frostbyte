e5ab5366c2a4511bd6ab40e9a06bb832bad41bbd
Theheavyduty <luongalex7@gmail.com>
2025-12-18 12:47:24 +0100

    Adding the final version of frontend

--- Frontend/types/child.ts
+++
@@ -1,6 +0,0 @@
-// Say to Alex if we need to call it User or Child.
-export interface ChildData{
-    id: string, // Same as parent ID type question.
-    name: string
-    // We need a right child model from backend?
-}
